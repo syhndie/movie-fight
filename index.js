b94a056bfcd1ee993b8e1aa438f7fdf01e9818fa
Cindy <syhndie2@gmail.com>
2021-01-01 09:34:24 -0600

    added basic search functionality

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('Hi there!');
+const fetchData = async (searchTerm) => {
+    const searchResponse = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apikey: 'b14b5ae4',
+            s: searchTerm,
+            type: 'movie'
+        }
+    });
+    return searchResponse.data.Search;
+};
+
+const onInput = async (event) => {
+    const movies = await fetchData(event.target.value);
+    console.log(movies);
+};
+
+const input = document.querySelector('input');
+
+input.addEventListener('input', debounce(onInput));
