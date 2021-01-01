6cc52a1580dc8351732e96efb324ad4377c04660
Cindy <syhndie2@gmail.com>
2021-01-01 09:33:56 -0600

    added basic search functionality

---
+++ utils.js
@@ -0,0 +1,17 @@
+//helper function to limit rate of firing of an argument function
+const debounce = (func, delay = 1500) => {
+    let timeoutId;
+    //...args means all arguments that the argument function might have
+    return (...args) => {
+        //if we have a function that was called inside setTimeout, and it 
+        //hasn't run yet, then cancel it
+        if (timeoutId) {
+            clearTimeout(timeoutId);
+        }
+        timeoutId = setTimeout(() => {
+            //func.apply allows us to apply all arguments that might be 
+            //passed to the function
+            func.apply(null, args);
+        }, delay);
+    }
+};
