b5e1d951bce1a1afd08841b514630145b9e274f9
Cindy <syhndie2@gmail.com>
2021-01-07 08:45:36 -0600

    movie details show when user selects a movie from a dropdown

--- index.js
+++ index.js
@@ -6,9 +6,6 @@ const autoCompleteConfig = {
             ${movie.Title} (${movie.Year})
        `;
     },
-    onOptionSelect: (movie) => {
-        onMovieSelect(movie);
-    },
     inputValue: (movie) => {
         return movie.Title;
     },
@@ -28,21 +25,29 @@ const autoCompleteConfig = {
 }
 createAutocomplete({
     ...autoCompleteConfig,
-    root: document.querySelector('#left-autocomplete')
+    root: document.querySelector('#left-autocomplete'),
+    onOptionSelect: (movie) => {
+        document.querySelector('.tutorial').classList.add('is-hidden');
+        onMovieSelect(movie, document.querySelector('#left-summary'));
+    }
 });
 createAutocomplete({
     ...autoCompleteConfig,
-    root: document.querySelector('#right-autocomplete')
+    root: document.querySelector('#right-autocomplete'),
+    onOptionSelect: (movie) => {
+        document.querySelector('.tutorial').classList.add('is-hidden');
+        onMovieSelect(movie, document.querySelector('#right-summary'));
+    }
 });
 
-const onMovieSelect = async (movie) => {
+const onMovieSelect = async (movie, summaryElement) => {
     const response = await axios.get('http://www.omdbapi.com/', {
         params: {
             apikey: 'b14b5ae4',
             i: movie.imdbID
         }
     });
-    document.querySelector("#summary").innerHTML = movieTemplate(response.data);
+    summaryElement.innerHTML = movieTemplate(response.data);
 }
 
 const movieTemplate = (movieDetail) => {
